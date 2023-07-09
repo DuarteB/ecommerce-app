abe5fac59d0e81c52f92df9c1df9b941d39701c1
DuarteB <eduarte.dev@gmail.com>
2023-07-09 00:34:26 -0300

    Finished the checkout page

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -5,21 +5,34 @@ import { CartContext } from "../../contexts/cart.context";
 import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem }) => {
-	const { addItemToCart } = useContext(CartContext);
+	const { addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(CartContext);
 
-	const increaseItemQuantity = () => {
+	const increaseItemHandler = () => {
 		addItemToCart(cartItem);
+	}
+
+	const deleteItemHandler = () => {
+		removeItemFromCart(cartItem);
+	}
+
+	const clearItemHandler = () => {
+		clearItemFromCart(cartItem);
 	}
 
 	const { name, price, imageUrl, quantity } = cartItem;
   return(
-		<div>
-			<img src={imageUrl}/>
-			<span>{name}</span>
-			<span>Qtd: {quantity}</span>
-			<span>x Total: ${price}</span>
-			<br></br>
-			<button onClick={increaseItemQuantity}>increase qtd</button>
+		<div className="checkout-item-container">
+			<div  className="image-container">
+				<img src={imageUrl} alt={`${name}`} />
+			</div>
+			<span className="name">{name}</span>
+			<span className="quantity">
+				<div className="arrow" onClick={deleteItemHandler}>&#10094;</div>
+				<span className="value">{quantity}</span>
+				<div className="arrow" onClick={increaseItemHandler}>&#10095;</div>
+			</span>
+			<span className="price">x ${price}</span>
+			<div className="remove-button" onClick={clearItemHandler}>&#10005;</div>
 		</div>
   )
 }
--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -2,13 +2,13 @@ import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems: any, productToAdd: any) => {
   // Find if cartItems contains productToAdd
-  const existingCartItem = cartItems.find((cartItem: { id: number }) =>
-    cartItem.id === productToAdd.id
+  const existingCartItem = cartItems.find(
+    (cartItem: { id: number }) => cartItem.id === productToAdd.id
   );
 
   // If found, increment quantity
   if (existingCartItem) {
-    return cartItems.map((cartItem: { id: number; quantity: number; }) =>
+    return cartItems.map((cartItem: { id: number; quantity: number }) =>
       cartItem.id === productToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
@@ -19,36 +19,87 @@ const addCartItem = (cartItems: any, productToAdd: any) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
-// const deleteCartItem = (cartItems, productToDelete) => {
-//   const newCart = 
-//   return [...newCart];
-// }
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  //Find the cart item to remove;
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  console.log(existingCartItem);
+
+  //Check if quantity is equal to 1, if it is remove that item from the cart;
+  if (existingCartItem && existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  //Return back cartItems with matching cart item with redeced quantity;
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+const clearCartItem = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
 export const CartContext = createContext({
   cartItems: [],
   addItemToCart: () => {},
   showDropdown: false,
   setShowDropdown: () => {},
-  cartCount: 0
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
+  total: 0,
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }: any) => {
   const [cartItems, setCartItems] = useState([]);
   const [showDropdown, setShowDropdown] = useState(false);
   const [cartCount, setCartCount] = useState(0);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    const newCartCount = cartItems.reduce((total, cartItem: { quantity: number }) => 
-      total + cartItem.quantity
-    , 0)
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => cartItem.price * cartItem.quantity + total,
+      0
+    );
+
+    setTotal(newCartTotal);
+  }, [cartItems]);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem: { quantity: number }) => total + cartItem.quantity,
+      0
+    );
+
     setCartCount(newCartCount);
-  }, [cartItems])
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd: any) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value: any = { cartItems, addItemToCart, showDropdown, setShowDropdown, cartCount };
+  const removeItemFromCart = (cartItemToRemove: any) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
+  const clearItemFromCart = (cartItemToClear: any) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+  };
+
+  const value: any = {
+    cartItems,
+    addItemToCart,
+    showDropdown,
+    setShowDropdown,
+    removeItemFromCart,
+    clearItemFromCart,
+    cartCount,
+    total,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
